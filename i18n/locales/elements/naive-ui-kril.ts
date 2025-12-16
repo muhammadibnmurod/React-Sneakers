369063db4e5165b4d1a286dc4e47c0a4a0ce615a
tohirovmuhammadsodiq <220481@centralasian.uz>
2025-12-16 14:39:06 +0500

    first commit

---
+++ i18n/locales/elements/naive-ui-kril.ts
@@ -0,0 +1,7 @@
+// locales/naive-kril.ts
+import type { NLocale, NDateLocale } from 'naive-ui'
+import krilJson from './naive-ui.kril.json'
+import krilDateJson from './naive-ui.kril.json'
+
+export const krilLocale = krilJson as unknown as NLocale
+export const krilDateLocale = krilDateJson as unknown as NDateLocale
