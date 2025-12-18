cb3269bd4b7ed60c60dc52486370382620b7f6a9
muhammadibnmurod <220481@centralasian.uz>
2025-12-18 11:54:54 +0500

    Refactor code structure for improved readability and maintainability

--- components/layouts/Sneakers.vue
+++ components/layouts/Sneakers.vue
@@ -19,9 +19,12 @@ import Sneaker1 from "@/assets/images/sneakers/snerakers1.png";
 import Sneaker2 from "@/assets/images/sneakers/snerakers2.png";
 import Sneaker3 from "@/assets/images/sneakers/snerakers3.png";
 import Sneaker4 from "@/assets/images/sneakers/snerakers4.png";
-
-
-
+import Sneaker5 from "@/assets/images/sneakers/snerakers5.png";
+import Sneaker6 from "@/assets/images/sneakers/snerakers6.png";
+import Sneaker7 from "@/assets/images/sneakers/snerakers7.png";
+import Sneaker8 from "@/assets/images/sneakers/snerakers8.png";
+import Sneaker9 from "@/assets/images/sneakers/snerakers9.png";
+import Sneaker10 from "@/assets/images/sneakers/snerakers10.png";
 
 interface Sneaker {
   id: number;
@@ -34,26 +37,74 @@ const sneakers: Sneaker[] = [
   {
     id: 1,
     title: "Мужские Кроссовки Nike Blazer Mid Suede",
-    price: 12999,
+    price: 12.999,
     imgSrc: Sneaker1,
   },
   {
-    id: 1,
+    id: 2,
     title: "Мужские Кроссовки Nike Air Max 270",
-    price: 12999,
+    price: 12.999,
     imgSrc: Sneaker2,
   },
   {
-    id: 1,
+    id: 3,
     title: "Мужские Кроссовки Nike Blazer Mid Suede",
-    price: 8400,
+    price: 8.400,
     imgSrc: Sneaker3,
   },
-    {
-    id: 1,
+  {
+    id: 4,
     title: "Кроссовки Puma X Aka Boku Future Rider",
-    price: 8999,
+    price: 8.999,
     imgSrc: Sneaker4,
+  },
+  {
+    id: 5,
+    title: "Мужские Кроссовки Under Armour Curry 8",
+    price: 15.119,
+    imgSrc: Sneaker5,
+  },
+  {
+    id: 6,
+    title: "Мужские Кроссовки Nike Kyrie 7",
+    price: 11.299,
+    imgSrc: Sneaker6,
+  },
+  {
+    id: 7,
+    title: "Мужские Кроссовки Jordan Air Jordan 11",
+    price: 10.799,
+    imgSrc: Sneaker7,
+  },
+  {
+    id: 8,
+    title: "Мужские Кроссовки Nike LeBron XVIII",
+    price: 16.499,
+    imgSrc: Sneaker8,
+  },
+  {
+    id: 9,
+    title: "Мужские Кроссовки Nike Lebron XVIII Low",
+    price: 13.999,
+    imgSrc: Sneaker9,
+  },
+  {
+    id: 10,
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8.499,
+    imgSrc: Sneaker1,
+  },
+  {
+    id: 11,
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8.999,
+    imgSrc: Sneaker4,
+  },
+  {
+    id: 12,
+    title: "Мужские Кроссовки Nike Kyrie Flytrap IV",
+    price: 11.299,
+    imgSrc: Sneaker10,
   },
 ];
 </script>
--- components/ui/card.vue
+++ components/ui/card.vue
@@ -1,25 +1,54 @@
 <template>
   <div class="p-4 rounded-2xl">
     <n-image :src="imgSrc" width="200" height="150" />
-    <h3 class="mt-2 font-inter text-[14px] font-[400] max-w-[205px]">{{ title }}</h3>
+
+    <h3 class="mt-2 font-inter text-[14px] font-[400] max-w-[205px]">
+      {{ title }}
+    </h3>
+
     <div class="mt-2 justify-between flex items-center w-full max-w-[205px]">
       <div>
-        <p>Narxi:</p>
-        <p class="text-gray-600">{{ price }} USD</p>
+        <p class="font-inter font-[500] text-[15px] text-[#8D8D8D]">Цена:</p>
+        <p class="font-inter font-bold text-xl">{{ price }} USD</p>
       </div>
+
       <div>
-        <button class=" cursor-pointer px-2 py-4">+</button>
+        <button class="cursor-pointer px-2 py-4" @click="handleClick">
+          <span v-if="!showImage"><img class="w-3 h-3 text-[#D3D3D3]" :src="Plus" alt="Loading..."></span>
+          <img v-else :src="SelectTrue" alt="loading..." class="w-5 h-5" />
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-interface props {
+import SelectTrue from "@/assets/icons/selectTrue.svg";
+import Plus from '@/assets/icons/Vector.png'
+
+const showImage = ref(false);
+let timeoutId: number | null = null;
+
+const handleClick = () => {
+  if (showImage.value) return;
+
+  showImage.value = true;
+
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+
+  timeoutId = window.setTimeout(() => {
+    showImage.value = false;
+    timeoutId = null;
+  }, 3000);
+};
+
+interface Props {
   title: string;
   price: number;
   imgSrc: string;
 }
 
-defineProps<props>();
+defineProps<Props>();
 </script>
