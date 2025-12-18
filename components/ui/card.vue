d0ab8bd1cec1c2a8d04a86040e1a8228ae77f1f9
muhammadibnmurod <220481@centralasian.uz>
2025-12-18 16:11:19 +0500

    feat: enhance card component with price display and image toggle functionality; add SVGO configuration

--- components/ui/card.vue
+++ components/ui/card.vue
@@ -6,16 +6,19 @@
       {{ title }}
     </h3>
 
-    <div class="mt-2 justify-between flex items-center w-full max-w-[205px]">
+    <div class="mt-2 justify-between flex items-center w-full max-w-[205px] min-h-5">
       <div>
         <p class="font-inter font-[500] text-[15px] text-[#8D8D8D]">Цена:</p>
         <p class="font-inter font-bold text-xl">{{ price }} USD</p>
       </div>
 
       <div>
-        <button class="cursor-pointer px-2 py-4" @click="handleClick">
-          <span v-if="!showImage"><img class="w-3 h-3 text-[#D3D3D3]" :src="Plus" alt="Loading..."></span>
-          <img v-else :src="SelectTrue" alt="loading..." class="w-5 h-5" />
+        <button class="cursor-pointer px-4 py-6 h-6" @click="handleClick">
+          <span v-if="!showImage"
+            ><img class="w-3 h-3 text-[#D3D3D3]" :src="Plus" alt="Loading..."
+          /></span>
+          <!-- <img :src="SelectTrue" alt="loading..." class="w-5 h-5" /> -->
+          <IconSelectTrue v-else class="dark:text-success-100" />
         </button>
       </div>
     </div>
@@ -23,8 +26,7 @@
 </template>
 
 <script setup lang="ts">
-import SelectTrue from "@/assets/icons/selectTrue.svg";
-import Plus from '@/assets/icons/Vector.png'
+import Plus from "@/assets/icons/Vector.png";
 
 const showImage = ref(false);
 let timeoutId: number | null = null;
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,6 +42,54 @@ export default defineNuxtConfig({
     langDir: "locales/",
   },
 
+  svgo: {
+    componentPrefix: "icon",
+    autoImportPath: "./assets/icons/",
+    defaultImport: "component",
+    svgoConfig: {
+      multipass: false, // Отключаем многоходовую оптимизацию
+      plugins: [
+        {
+          name: "preset-default",
+          params: {
+            overrides: {
+              // Отключаем ВСЕ плагины оптимизации
+              removeDoctype: false,
+              removeXMLProcInst: false,
+              removeComments: false,
+              removeMetadata: false,
+              removeEditorsNSData: false,
+              cleanupIds: false,
+              convertColors: false,
+              convertPathData: false,
+              convertTransform: false,
+              removeUnknownsAndDefaults: false,
+              removeNonInheritableGroupAttrs: false,
+              removeUselessStrokeAndFill: false,
+              removeUnusedNS: false,
+              cleanupNumericValues: false,
+              moveElemsAttrsToGroup: false,
+              moveGroupAttrsToElems: false,
+              collapseGroups: false,
+              mergePaths: false,
+              convertShapeToPath: false,
+              sortAttrs: false,
+              removeTitle: false,
+              removeDesc: false,
+              removeEmptyContainers: false,
+              removeViewBox: false,
+              cleanupEnableBackground: false,
+              removeHiddenElems: false,
+              removeEmptyText: false,
+              inlineStyles: false,
+              minifyStyles: false
+            }
+          }
+        }
+      ]
+    }
+  },
+
   imports: {
     autoImport: true,
   },
