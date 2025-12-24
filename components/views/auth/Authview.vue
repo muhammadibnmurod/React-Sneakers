6a7fb724655806e3a915c2dd2879723b7314080d
tohirovmuhammadsodiq <220481@centralasian.uz>
2025-12-24 11:19:18 +0500

    refactor: remove unused API composables
    - Deleted `useApi.ts` and `useApiService.ts` as they are no longer needed in the project.

--- components/views/auth/Authview.vue
+++ components/views/auth/Authview.vue
@@ -1,34 +1,58 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import AuthCarousel from "./AuthCarousel.vue";
+
+const form = ref({
+  email: "",
+  password: "",
+  remember: false,
+});
 </script>
 
 <template>
-  <h1
-    class="flex w-full h-[850px] bg-surface-40 rounded-2xl border border-surface-30 shadow-auth-login font-geist"
-  >
-    <div class="flex flex-1 items-center justify-center px-10 h-full">
-      <div class="flex flex-col items-center gap-8">
+  <div
+    class="flex w-full h-[850px] bg-surface-40 rounded-2xl border border-surface-30 shadow-auth-login font-geist overflow-hidden">
+    <div class="flex flex-1 items-center justify-center px-10 h-full bg-white">
+      <div class="flex flex-col items-center gap-10 w-full max-w-[420px]">
         <img src="@/assets/images/Logo.png" class="w-10 h-10" />
-        <div class="flex flex-col gap-4 items-center">
-          <h2 class="font-bold text-[40px]">Sneakers</h2>
-          <p class="text-[20px] text-[#5E5D5D]">
-            Welcome to the world knowledge
+
+        <div class="flex flex-col gap-3 items-center text-center">
+          <h2 class="font-bold text-[40px]">Opus</h2>
+          <p class="text-[18px] text-[#5E5D5D]">
+            Welcome to the world of knowledge
           </p>
         </div>
-        <div>
+
+        <n-form :model="form" class="w-full" size="large">
           <n-form-item>
-            <n-input />
+            <n-input v-model:value="form.email" placeholder="Email" type="email" class="custom-input" />
           </n-form-item>
-        </div>
+
+          <n-form-item>
+            <n-input v-model:value="form.password" placeholder="Password" type="password" show-password-on="click" />
+          </n-form-item>
+
+          <n-button type="primary" size="large" class="w-full !bg-[#111827] !rounded-lg text-white">
+            Sign in
+          </n-button>
+        </n-form>
+
+        <p class="text-sm text-gray-500">
+          Don’t have an account?
+          <a href="#" class="font-semibold text-black">Sign up</a>
+        </p>
       </div>
     </div>
 
-    <div class="flex-1 h-full flex items-center">
-      <div class="w-full h-full flex items-center p-1 rounded">
-        <AuthCarousel />
-      </div>
+    <div class="flex-1 h-full">
+      <AuthCarousel />
     </div>
-  </h1>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.custom-input {
+  --n-placeholder-color: #000000;
+  --n-text-color: #000000;
+}
+</style>
